1684008c640307308688de71c14b16509ed95814
Jeremy Granadillo <akakitsu@gmail.com>
2014-12-23 12:18:58 +0100

    Make sure API keys update after info is fetched

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -6,23 +6,28 @@ define(
 
     controllers.controller ('dashboard',
       [
-        '$scope', '$rootScope', 'AskFast', 'Session', 'Store',  
+        '$scope', '$rootScope', 'AskFast', 'Session', 'Store',
         function ($scope, $rootScope, AskFast, Session, Store )
         {
-            AskFast.caller('info')
-              .then(function (info)
-				{
-					AskFast.caller('key')
-						.then(function (keys)
-					{
-						info.refreshToken = keys.refreshToken;
-					});
-					Store('app').save({
-                        user: info
-                    });
-				});
-			
-			
+          AskFast.caller('info')
+          .then(function (info)
+          {
+            AskFast.caller('key')
+            .then(function (keys)
+            {
+              info.refreshToken = keys.refreshToken;
+
+              Store('app').save({
+                user: info
+              });
+
+              $scope.$apply(function(scope){
+                $rootScope.user = Store('app').get('user');
+              });
+            });
+
+          });
+
         }
       ]
     );
