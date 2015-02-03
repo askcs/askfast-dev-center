19863e42f5d2e41d54bf69aa6d2f209bc346849b
Jeremy Granadillo <akakitsu@gmail.com>
2015-02-03 15:52:04 +0100

    Add simple unit test

--- karma.conf.js
+++ karma.conf.js
@@ -5,32 +5,52 @@ module.exports = function (karma)
   karma.set({
     frameworks: ['jasmine', 'requirejs'],
     files: [
-      {pattern: 'app/vendors/**/*.js',        included: false},
+      {pattern: 'app/vendors/jquery/jquery.min.js',        included: false},
+      {pattern: 'app/vendors/angular/angular.js',        included: false},
+      {pattern: 'app/vendors/angular-mocks/angular-mocks.js',        included: false},
+      {pattern: 'app/vendors/bootstrap-sass/dist/js/bootstrap.min.js',        included: false},
+      {pattern: 'app/vendors/angular-strap/dist/angular-strap.min.js',        included: false},
+      {pattern: 'app/vendors/angular-strap/dist/angular-strap.tpl.min.js',        included: false},
+      {pattern: 'app/vendors/angular-resource/angular-resource.min.js',        included: false},
+      {pattern: 'app/vendors/angular-route/angular-route.min.js',        included: false},
+      {pattern: 'app/vendors/lawnchair/src/Lawnchair.js',        included: false},
+      {pattern: 'app/vendors/lawnchair/src/adapters/dom.js',        included: false},
+      {pattern: 'app/vendors/momentjs/min/moment.min.js',        included: false},
+      {pattern: 'app/vendors/angular-datatables/dist/angular-datatables.js',        included: false},
+      // {pattern: 'app/vendors/**/*.js',        included: false},
       {pattern: 'app/scripts/*.js',           included: false},
       {pattern: 'app/scripts/**/*.js',        included: false},
+      {pattern: 'app/scripts/**/*.coffee',        included: false},
       {pattern: 'test/spec/controllers/*.js', included: false},
-      {pattern: 'test/spec/directives/*.js',  included: false},
-      {pattern: 'test/spec/filters/*.js',     included: false},
-      {pattern: 'test/spec/services/*.js',    included: false},
+      // {pattern: 'test/spec/directives/*.js',  included: false},
+      // {pattern: 'test/spec/filters/*.js',     included: false},
+      // {pattern: 'test/spec/services/*.js',    included: false},
       'test/spec/test-unit-main.js'
     ],
     basePath: '',
-    exclude: [],
-    reporters: ['progress'],
+    exclude: [
+      'app/scripts/main.js',
+      'app/scripts/libs/**/*Spec.js'
+    ],
     port: 8080,
-    runnerPort: 9100,
-    colors: true,
     logLevel: karma.LOG_INFO,
-    autoWatch: true,
+    autoWatch: false,
     browsers: ['PhantomJS'],
-    captureTimeout: 5000,
+    captureTimeout: 10000,
     singleRun: false,
-    plugins: [
-      'karma-jasmine',
-      'karma-requirejs',
-      'karma-chrome-launcher',
-      'karma-firefox-launcher',
-      'karma-phantomjs-launcher'
-    ]
+    preprocessors: {
+      '**/*.coffee': ['coffee']
+    },
+    coffeePreprocessor: {
+      // options passed to the coffee compiler
+      options: {
+        bare: true,
+        map: false
+      },
+      // transforming the filenames
+      transformPath: function(path) {
+        return path.replace(/\.coffee$/, '.js');
+      }
+    }
   });
 };
---
+++ test/spec/controllers/coreSpec.js
@@ -0,0 +1,89 @@
+define(
+  [
+    'angular-mocks',
+    'app',
+    'controllers/core',
+    'services/moment'
+  ],
+  function(ngMock, app){
+    'use strict';
+
+    // Bind polyfill for PhantomJS
+    // TODO: Load polyfill by default
+    beforeAll(function(){
+      var isFunction = function(o) {
+        return typeof o == 'function';
+      };
+
+      var bind,
+        slice = [].slice,
+        proto = Function.prototype,
+        featureMap;
+
+      featureMap = {
+        'function-bind': 'bind'
+      };
+
+      function has(feature) {
+        var prop = featureMap[feature];
+        return isFunction(proto[prop]);
+      }
+
+      // check for missing features
+      if (!has('function-bind')) {
+        // adapted from Mozilla Developer Network example at
+        // https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Function/bind
+        bind = function bind(obj) {
+          var args = slice.call(arguments, 1),
+            self = this,
+            nop = function() {
+            },
+            bound = function() {
+              return self.apply(this instanceof nop ? this : (obj || {}), args.concat(slice.call(arguments)));
+            };
+          nop.prototype = this.prototype || {}; // Firefox cries sometimes if prototype is undefined
+          bound.prototype = new nop();
+          return bound;
+        };
+        proto.bind = bind;
+      }
+    });
+
+    beforeEach(module('DevCen'));
+
+    beforeEach(function(){
+      var mockAskFast = {
+        caller: function(){
+          return {then: function(){return "nothing";}};
+        }
+      };
+      var mockStore = function(thing){
+        return {
+          get: function(){},
+        };
+      };
+
+      module('controllers', function($provide) {
+        $provide.value('AskFast', mockAskFast);
+        $provide.value('Store', mockStore);
+      })
+    });
+
+    describe('The core controller:', function(){
+
+      var coreCtrl, scope;
+
+      beforeEach(inject(function($controller, $rootScope){
+        scope = $rootScope.$new();
+        coreCtrl = $controller('core', {
+          $scope: scope
+        });
+      }));
+
+      it('should contain $scope.loading', function(){
+        expect(typeof scope.loading).not.toBe('undefined');
+      });
+
+    });
+
+  })
